a17ef1d8426556abeaa212b6abad5b1136f55ea3
Rachel Jensen <rachelangela@gmail.com>
2021-07-12 16:08:32 -0600

    Add User constructor

--- src/User.js
+++ src/User.js
@@ -1,3 +1,17 @@
-class User {}
-
+class User {
+  constructor(userData) {
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
+  }
+}
+//userData is the parameter
 export default User;
+
+// should have a paramter to take in userData object
+// object property key for each of the keys in the userData object
+// "id","name","address","email","strideLength","dailyStepGoal","friends"
+// getFirstName(); method to return first name
