a0031c067db0044aea93027ff6b017bbb6fbf9d3
Rachel Jensen <rachelangela@gmail.com>
2021-07-14 16:18:28 -0600

    Refactor fetch call for user data

--- src/scripts.js
+++ src/scripts.js
@@ -15,23 +15,11 @@ import './images/running.png';
 
 // console.log('This is the JavaScript entry file - your code begins here.');
 
-//FETCH CALL 1
-
-// const printData = () => {
-//   apiData1.then((a) => {
-//     console.log(a);
-//     return a;
-//   });
-// };
-
-// let check = printData();
-// console.log(check);
-
 // global variables 👇
-// instantiate UserRepo
+
 let currentUser;
-let userRepo; /* = new UserRepository(userData); */
-let fetchSleepData;
+let userRepo;
+//let fetchSleepData;
 
 // query selectors 👇
 const userGreeting = document.getElementById('userGreeting');
@@ -47,37 +35,20 @@ const friend3 = document.getElementById('friend3');
 window.addEventListener('load', loadUserData);
 
 // functions: handlers and helpers 👇
-function getData() {
-  fetch('http://localhost:3001/api/v1/users')
-    .then((response) => response.json())
-    .then((data) => {
-      userRepo = new UserRepository(data.userData);
-      userRepo.instantiateAllUsers();
-      currentUser = userRepo.getUserInfo(45);
-      displayProfileBox();
-      // console.log(userRepo);
-      // // return userRepo;
-    });
-  // return userRepo;
-  // .catch((err) => console.log('oh butts'));
-}
 
 function loadUserData() {
   getAllData().then((data) => {
-    userRepo = new UserRepository(data[0].userData);
-    console.log(userRepo);
-    //userRepo = new UserRepository(data.userData);
-    userRepo.instantiateAllUsers();
-    currentUser = userRepo.getUserInfo(45);
-    displayProfileBox();
+    //this is retrieving the array of data array from Promise.all in apiCalls.js
+    getCurrentUser(data[0].userData);
+    displayProfileBox(); //DOM
   });
-  // getData();
-  // userRepo.instantiateAllUsers();
-  // currentUser = userRepo.getUserInfo(45);
-  // displayProfileBox();
 }
 
-console.log('check', userRepo);
+function getCurrentUser(parsedData) {
+  userRepo = new UserRepository(parsedData);
+  userRepo.instantiateAllUsers();
+  currentUser = userRepo.getUserInfo(45);
+}
 
 function displayProfileBox() {
   userGreeting.innerText = currentUser.getFirstName();
