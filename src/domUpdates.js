6e02c126ec75f2c6e0c7928ac2dbed0eb05c28f3
Rachel Jensen <rachelangela@gmail.com>
2021-07-14 15:31:33 -0600

    Add domUpdates.js file

---
+++ src/domUpdates.js
@@ -0,0 +1,14 @@
+// let domUpdates = {
+//   displayProfileBox() {
+//     userGreeting.innerText = currentUser.getFirstName();
+//     address.innerText = currentUser.address;
+//     email.innerText = currentUser.email;
+//     stepGoal.innerText = currentUser.dailyStepGoal;
+//     strideLength.innerText = currentUser.strideLength;
+//     friend1.innerText = userRepo.allUsers[currentUser.friends[0]].getFirstName();
+//     friend2.innerText = userRepo.allUsers[currentUser.friends[1]].getFirstName();
+//     friend3.innerText = userRepo.allUsers[currentUser.friends[2]].getFirstName();
+//   }
+// };
+
+// export default domUpdates;
