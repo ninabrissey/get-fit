f3d9185d0f4bcf3b809f6c8cbec8cd08180505a0
Nina Brissey <nbrissey@gmail.com>
2021-07-13 18:14:29 -0700

    Add profile display

--- src/scripts.js
+++ src/scripts.js
@@ -15,26 +15,36 @@ import './images/running.png';
 
 // global variables 👇
 // instantiate UserRepo
-const allUsers = new UserRepository(userData);
-// instantiate Jennie via getUserInfo(45);
-const currentUser = allUsers.getUserInfo(45);
+const userRepo = new UserRepository(userData);
+let currentUser;
 
 // query selectors 👇
 const userGreeting = document.getElementById('userGreeting');
 const address = document.getElementById('address');
 const email = document.getElementById('email');
-const stepGoal = document.getElementById('stepGoals');
+const stepGoal = document.getElementById('stepGoal');
 const strideLength = document.getElementById('strideLength');
 const friend1 = document.getElementById('friend1');
 const friend2 = document.getElementById('friend2');
 const friend3 = document.getElementById('friend3');
 
 // event listeners 👇
+window.addEventListener('load', loadUserData);
 
 // functions: handlers and helpers 👇
+function loadUserData() {
+  userRepo.instantiateAllUsers();
+  currentUser = userRepo.getUserInfo(45);
+  displayProfileBox();
+}
 
-// We are writing all of this as if our frined Jennie has signed in the app
-
-// determine how the initial page display is activated (as a page load listener?)
-// initial display -->
-//
+function displayProfileBox() {
+  userGreeting.innerText = currentUser.getFirstName();
+  address.innerText = currentUser.address;
+  email.innerText = currentUser.email;
+  stepGoal.innerText = currentUser.dailyStepGoal;
+  strideLength.innerText = currentUser.strideLength;
+  friend1.innerText = userRepo.allUsers[currentUser.friends[0]].getFirstName();
+  friend2.innerText = userRepo.allUsers[currentUser.friends[1]].getFirstName();
+  friend3.innerText = userRepo.allUsers[currentUser.friends[2]].getFirstName();
+}
