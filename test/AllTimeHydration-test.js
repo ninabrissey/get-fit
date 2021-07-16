2b00506fb15429917c8e7b14f7679ca4a02f02a5
Nina Brissey <nbrissey@gmail.com>
2021-07-15 20:11:29 -0700

    Build AllTimeHydrsation tests

--- test/AllTimeHydration-test.js
+++ test/AllTimeHydration-test.js
@@ -16,7 +16,7 @@ describe('AllTimeHydration', () => {
       {
         userID: 2,
         date: '2019/06/15',
-        numOunces: 75,
+        numOunces: 50,
       },
       {
         userID: 1,
@@ -26,7 +26,7 @@ describe('AllTimeHydration', () => {
       {
         userID: 2,
         date: '2019/06/14',
-        numOunces: 37,
+        numOunces: 80,
       },
       {
         userID: 1,
@@ -36,71 +36,70 @@ describe('AllTimeHydration', () => {
       {
         userID: 2,
         date: '2019/06/13',
-        numOunces: 47,
+        numOunces: 100,
       },
       {
         userID: 1,
         date: '2019/06/12',
-        numOunces: 37,
+        numOunces: 56,
       },
       {
         userID: 2,
         date: '2019/06/12',
-        numOunces: 75,
+        numOunces: 28,
       },
       {
         userID: 1,
         date: '2019/06/11',
-        numOunces: 47,
+        numOunces: 49,
       },
       {
         userID: 2,
         date: '2019/06/11',
-        numOunces: 37,
+        numOunces: 39,
       },
       {
         userID: 1,
         date: '2019/06/10',
-        numOunces: 75,
+        numOunces: 30,
       },
       {
         userID: 2,
         date: '2019/06/10',
-        numOunces: 47,
+        numOunces: 29,
       },
       {
         userID: 1,
         date: '2019/06/09',
-        numOunces: 37,
+        numOunces: 64,
       },
       {
         userID: 2,
         date: '2019/06/09',
-        numOunces: 75,
+        numOunces: 20,
       },
       {
         userID: 1,
         date: '2019/06/08',
-        numOunces: 47,
+        numOunces: 48,
       },
       {
         userID: 2,
         date: '2019/06/08',
-        numOunces: 37,
+        numOunces: 36,
       },
       {
         userID: 1,
         date: '2019/06/07',
-        numOunces: 75,
+        numOunces: 76,
       },
       {
         userID: 2,
         date: '2019/06/07',
-        numOunces: 47,
+        numOunces: 87,
       },
     ];
-
-    allTimeHydration = new AllTimeHydration();
+    allTimeHydration = new AllTimeHydration(hydrationData);
   });
 
   it('should be a function', () => {
@@ -113,10 +112,11 @@ describe('AllTimeHydration', () => {
 
   it("should start with an array of all user's hydration data", () => {
     expect(allTimeHydration.allUsersHydration).to.be.a('array');
+    expect(allTimeHydration.allUsersHydration[7].numOunces).to.equal(28);
   });
 
   it('should start with an empty individual user hydration array', () => {
-    expect(allTimeHydration.individualHydration).to.equal([]);
+    expect(allTimeHydration.individualHydration).to.be.a('array');
   });
 
   it('should be able populate user array with dailyHydration objects for that user', () => {
@@ -132,21 +132,68 @@ describe('AllTimeHydration', () => {
   });
 
   it("should be able to calculate an individual's average daily ounces", () => {
+    allTimeHydration.getIndividualHydration(1);
     // second method total of all ounces divided by length of array
     let average = allTimeHydration.calculateAvgOunces();
-
-    expect(average).to.equal(53);
+    //need to use Math.round
+    expect(average).to.equal(54);
   });
 
   it('should be able to return daily ounces for a seven day period', () => {
-    //third method
+    allTimeHydration.getIndividualHydration(1);
+
+    const weeksHydration = allTimeHydration.getWeeklyHydration('2019/06/15');
+
+    expect(weeksHydration[3]).to.deep.equal({
+      id: 1,
+      date: '2019/06/12',
+      numOunces: 56,
+    });
+    //most recent date is last in the array
   });
 
-  it('should be able to return daily ounces for a different seven day period', () => {});
+  it.only('should be able to return daily ounces for a different seven day period', () => {
+    allTimeHydration.getIndividualHydration(2);
 
-  it('should always return an array with a length of seven', () => {});
+    const weeksHydration = allTimeHydration.getWeeklyHydration('2019/06/09');
 
-  it('should', () => {});
+    expect(weeksHydration).to.deep.equal([87, 36, 20, 29, 39, 28, 100]);
+    //most recent date is last in the array
+  });
 
-  it('should', () => {});
+  it('should return the daily ounces array even if the length of the array is less than seven', () => {
+    hydrationData = [
+      {
+        userID: 1,
+        date: '2019/06/15',
+        numOunces: 37,
+      },
+      {
+        userID: 2,
+        date: '2019/06/15',
+        numOunces: 50,
+      },
+      {
+        userID: 1,
+        date: '2019/06/14',
+        numOunces: 47,
+      },
+      {
+        userID: 2,
+        date: '2019/06/14',
+        numOunces: 80,
+      },
+      {
+        userID: 1,
+        date: '2019/06/13',
+        numOunces: 75,
+      },
+    ];
+    //if the length of the array is less then 7 or less return the whole array
+    allTimeHydration.getIndividualHydration(1);
+
+    const weeksHydration = allTimeHydration.getWeeklyHydration('2019/06/14');
+
+    expect(weeksHydration).to.deeply.equal([75, 47, 37]);
+  });
 });
