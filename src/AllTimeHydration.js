8a76e6da950e56faf2e929edf70e00862135a750
Nina Brissey <nbrissey@gmail.com>
2021-07-15 20:09:57 -0700

    Add class structure and methods logic

--- src/AllTimeHydration.js
+++ src/AllTimeHydration.js
@@ -1,7 +1,41 @@
 import DailyHydration from './DailyHydration';
 
 class AllTimeHydration {
-  constructor() {}
+  constructor(data) {
+    this.allUsersHydration = data;
+    this.individualHydration = [];
+  }
+
+  getIndividualHydration(id) {
+    this.individualHydration = this.allUsersHydration.reduce(
+      (array, element) => {
+        let dailyHydration;
+        if (element.userID === id) {
+          dailyHydration = new DailyHydration(element);
+          array.push(dailyHydration);
+        }
+        return array;
+      },
+      []
+    );
+  }
+
+  calculateAvgOunces() {
+    let total = 0;
+    this.individualHydration.forEach((element) => {
+      total += element.numOunces;
+    });
+    return Math.round(total / this.individualHydration.length);
+  }
+
+  getWeeklyHydration(date) {
+    console.log(this.individualHydration);
+    const i = this.individualHydration.findIndex(
+      (element) => element.date === date
+    );
+    console.log(i);
+    return this.individualHydration.slice(i, i + 7);
+  }
 }
 //array of all users/all days this.allUsersHydration
 //this.individualHydration
