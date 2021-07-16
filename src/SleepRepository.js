b527f2f12f847ef15d2c026f574b4ec341eaa9f0
Rachel Jensen <rachelangela@gmail.com>
2021-07-16 14:02:10 -0600

    Remove comments

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -48,34 +48,4 @@ class SleepRepository {
   }
 }
 
-/* 
-For a user, their sleep quality for a specific day (identified by a date)
-For a user, how many hours they slept for a specific day (identified by a date)
-
-For a user (identified by their userID), the average number of hours slept per day
-
-For a user, their average sleep quality per day over all time
-
-For a user, their sleep quality each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-
-For a user, how many hours slept each day over the course of a given week (7 days) - you should be able to calculate this for any week, not just the latest week
-
-For all users, the average sleep quality
-*/
 export default SleepRepository;
-
-/* 
-  getIndividualHydration(id) {
-    this.individualHydration = this.allUsersHydration.reduce(
-      (array, element) => {
-        let dailyHydration;
-        if (element.userID === id) {
-          dailyHydration = new DailyHydration(element);
-          array.push(dailyHydration);
-        }
-        return array;
-      },
-      []
-    );
-  }
-*/
