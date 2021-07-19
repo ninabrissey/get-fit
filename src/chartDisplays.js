c2c3f42d3b2ed100f2fad16b1d445ce27ed6ecc3
Nina Brissey <nbrissey@gmail.com>
2021-07-18 18:22:40 -0700

    Add sleep charts

--- src/chartDisplays.js
+++ src/chartDisplays.js
@@ -2,8 +2,7 @@ import { Chart } from 'chart.js';
 Chart.defaults.font.family = "'Oswald', sans-serif";
 
 export function makeDailyHydrationChart(dayData) {
-  const dailyWaterChartX = document.getElementById('dailyWaterChart');
-  const dailyWaterChart = new Chart(dailyWaterChartX, {
+  const config = {
     type: 'doughnut',
     data: {
       labels: ['Water Intake', 'Water Needed'],
@@ -15,7 +14,9 @@ export function makeDailyHydrationChart(dayData) {
         },
       ],
     },
-  });
+  };
+  const dailyWaterChartX = document.getElementById('dailyWaterChart');
+  const dailyWaterChart = new Chart(dailyWaterChartX, config);
 }
 
 export function makeWeeklyHydrationChart(weekOfWater) {
@@ -53,4 +54,140 @@ export function makeWeeklyHydrationChart(weekOfWater) {
   };
   const weeklyWaterChart = document.getElementById('weeklyWaterChart'); //added for test
   new Chart(weeklyWaterChart, config);
+}
+
+export function makeNightsSleepChart(sleep) {
+  const data = {
+    labels: [''],
+    datasets: [
+      {
+        label: 'Last Night',
+        data: [sleep.value],
+        borderColor: 'white',
+        backgroundColor: 'black',
+      },
+      {
+        label: 'Average',
+        data: [sleep.average],
+        borderColor: 'white',
+        backgroundColor: 'red',
+      },
+    ],
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      indexAxis: 'y',
+      // Elements options apply to all of the options unless overridden in a dataset
+      // In this case, we are setting the border of each horizontal bar to be 2px wide
+      elements: {
+        bar: {
+          borderWidth: 2,
+        },
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'right',
+        },
+        title: {
+          display: true,
+          text: 'Hours Slept',
+        },
+      },
+    },
+  };
+
+  //const NUMBER_CFG = { count: 1, min: 0, max: 12 };
+
+  //const labels = Utils.months({ count: 7 });
+  const sleepChart = document.getElementById('sleepChart'); //added for test
+  new Chart(sleepChart, config);
+}
+
+export function makeNightsQualityChart(sleep) {
+  const data = {
+    labels: [''],
+    datasets: [
+      {
+        label: 'Last Night',
+        data: [sleep.value],
+        borderColor: 'white',
+        backgroundColor: 'black',
+      },
+      {
+        label: 'Average',
+        data: [sleep.average],
+        borderColor: 'white',
+        backgroundColor: 'red',
+      },
+    ],
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      indexAxis: 'y',
+      // Elements options apply to all of the options unless overridden in a dataset
+      // In this case, we are setting the border of each horizontal bar to be 2px wide
+      elements: {
+        bar: {
+          borderWidth: 2,
+        },
+      },
+      // responsive: true,
+      plugins: {
+        legend: {
+          position: 'right',
+        },
+        title: {
+          display: true,
+          text: 'Sleep Quality',
+        },
+      },
+    },
+  };
+
+  const sleepScoreChart = document.getElementById('sleepScoreChart'); //added for test
+  new Chart(sleepScoreChart, config);
+}
+
+export function makeWeeksSleepChart(weekOfSleep) {
+  const labels2 = weekOfSleep.map((day) => day.date);
+  const data = {
+    labels: labels2,
+    datasets: [
+      {
+        label: 'Hours of Sleep Per Night',
+        data: weekOfSleep.map((day) => day.hoursSlept),
+        backgroundColor: [
+          '#4CDB63',
+          '#2BBFAB',
+          '#0391FA',
+          '#6542DC',
+          '#FF2B55',
+          '#FF5B28',
+          '#FEB904',
+        ],
+        borderColor: 'white',
+        borderWidth: 1,
+      },
+    ],
+  };
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+  const weeklySleepChart = document.getElementById('weeklySleepChart'); //added for test
+  new Chart(weeklySleepChart, config);
 }
