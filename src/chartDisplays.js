496e69f0152bed9d450101e2c3b8ebd4a5501e28
Nina Brissey <nbrissey@gmail.com>
2021-07-20 14:20:11 -0700

    Change functions to es6

--- src/chartDisplays.js
+++ src/chartDisplays.js
@@ -1,7 +1,7 @@
 import { Chart } from 'chart.js';
 Chart.defaults.font.family = "'Oswald', sans-serif";
 
-export function makeDailyHydrationChart(dayData) {
+export const makeDailyHydrationChart = (dayData) => {
   const config = {
     type: 'doughnut',
     data: {
@@ -19,9 +19,9 @@ export function makeDailyHydrationChart(dayData) {
   };
   const dailyWaterChartX = document.getElementById('dailyWaterChart');
   const dailyWaterChart = new Chart(dailyWaterChartX, config);
-}
+};
 
-export function makeWeeklyHydrationChart(weekOfWater) {
+export const makeWeeklyHydrationChart = (weekOfWater) => {
   const labels2 = weekOfWater.map((day) => day.date);
   const data = {
     labels: labels2,
@@ -39,7 +39,7 @@ export function makeWeeklyHydrationChart(weekOfWater) {
           '#4CDB63',
         ],
         borderColor: 'white',
-        borderWidth: 1,
+        borderWidth: 2,
         borderRadius: Number.MAX_VALUE,
       },
     ],
@@ -57,9 +57,9 @@ export function makeWeeklyHydrationChart(weekOfWater) {
   };
   const weeklyWaterChart = document.getElementById('weeklyWaterChart'); //added for test
   new Chart(weeklyWaterChart, config);
-}
+};
 
-export function makeNightsSleepChart(sleep) {
+export const makeNightsSleepChart = (sleep) => {
   const data = {
     labels: [''],
     datasets: [
@@ -83,8 +83,6 @@ export function makeNightsSleepChart(sleep) {
     data: data,
     options: {
       indexAxis: 'y',
-      // Elements options apply to all of the options unless overridden in a dataset
-      // In this case, we are setting the border of each horizontal bar to be 2px wide
       elements: {
         bar: {
           borderWidth: 2,
@@ -108,9 +106,9 @@ export function makeNightsSleepChart(sleep) {
   //const labels = Utils.months({ count: 7 });
   const sleepChart = document.getElementById('sleepChart'); //added for test
   new Chart(sleepChart, config);
-}
+};
 
-export function makeNightsQualityChart(sleep) {
+export const makeNightsQualityChart = (sleep) => {
   const data = {
     labels: [''],
     datasets: [
@@ -156,9 +154,9 @@ export function makeNightsQualityChart(sleep) {
 
   const sleepScoreChart = document.getElementById('sleepScoreChart'); //added for test
   new Chart(sleepScoreChart, config);
-}
+};
 
-export function makeWeeksSleepChart(weekOfSleep) {
+export const makeWeeksSleepChart = (weekOfSleep) => {
   const labels2 = weekOfSleep.map((day) => day.date);
   const data = {
     labels: labels2,
@@ -176,7 +174,7 @@ export function makeWeeksSleepChart(weekOfSleep) {
           '#4CDB63',
         ],
         borderColor: 'white',
-        borderWidth: 1,
+        borderWidth: 2,
         borderRadius: Number.MAX_VALUE,
       },
     ],
@@ -194,4 +192,4 @@ export function makeWeeksSleepChart(weekOfSleep) {
   };
   const weeklySleepChart = document.getElementById('weeklySleepChart'); //added for test
   new Chart(weeklySleepChart, config);
-}
+};
