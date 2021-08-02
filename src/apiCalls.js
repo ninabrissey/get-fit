ef4a07c0b4a8b4cdde460b3fc6290879a3bd89bb
Nina Brissey <nbrissey@gmail.com>
2021-08-02 15:37:26 -0700

    Create a reusable function to fetch get data from api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,29 +1,16 @@
-const getData = () => {
-  return fetch('http://localhost:3001/api/v1/users').then((response) =>
-    response.json()
-  );
-};
-
-const getHydration = () => {
-  return fetch('http://localhost:3001/api/v1/hydration').then((response) =>
-    response.json()
-  );
-};
-
-const getSleep = () => {
-  return fetch('http://localhost:3001/api/v1/sleep').then((response) =>
-    response.json()
-  );
-};
-
-const getActivity = () => {
-  return fetch('http://localhost:3001/api/v1/activity').then((response) =>
+const getData = (type) => {
+  return fetch(`http://localhost:3001/api/v1/${type}`).then((response) =>
     response.json()
   );
 };
 
 const getAllData = () => {
-  return Promise.all([getData(), getHydration(), getSleep(), getActivity()]);
+  return Promise.all([
+    getData('users'),
+    getData('hydration'),
+    getData('sleep'),
+    getData('activity'),
+  ]);
 };
 
 export default getAllData;
