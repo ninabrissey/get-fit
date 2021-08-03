880e26c3254ac62f30e23b1c5b8d4396dd50dbf0
Nina Brissey <nbrissey@gmail.com>
2021-08-02 17:20:11 -0700

    Add post method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,4 +13,12 @@ const getAllData = () => {
   ]);
 };
 
+const postData = (postObject, type) => {
+  fetch(`http://localhost:3001/api/v1/${type}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(postObject),
+  });
+};
+
 export default getAllData;
